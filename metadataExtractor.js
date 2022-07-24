549a1aa2dc0004e4d686a57a9c0dbf1c0ae5c8c8
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-07-24 21:03:00 +0300

    all done

--- metadataExtractor.js
+++ metadataExtractor.js
@@ -18,9 +18,6 @@ const upload = multer({ storage: storage })
 
 // const upload = multer({ dest: './public/data/uploads/', preservePath: true })
 router.post('/', upload.single('uploaded_file'), async (req, res) => {
-    // req.file is the name of your file in the form above, here 'uploaded_file'
-    // req.body will hold the text fields, if there were any 
-    // console.log(req.file, req.body)
     console.log(req.file);
     const metadata = await metadataExtract(req.file.path);
     res.json(metadata);
