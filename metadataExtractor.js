10d9eaf5039ab0dee89dcab22742f87cfc9128de
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-07-24 21:13:03 +0300

    finally I'm done for now

--- metadataExtractor.js
+++ metadataExtractor.js
@@ -1,7 +1,7 @@
 const metadataExtract = require("metadata-extract");
 const router = require("express").Router();
 
-const multer = require('multer')
+const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -10,15 +10,13 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
-})
+});
 
 
 //using multer to upload files
 const upload = multer({ storage: storage })
 
-// const upload = multer({ dest: './public/data/uploads/', preservePath: true })
 router.post('/', upload.single('uploaded_file'), async (req, res) => {
-    console.log(req.file);
     const metadata = await metadataExtract(req.file.path);
     res.json(metadata);
 });
