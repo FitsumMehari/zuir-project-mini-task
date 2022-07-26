9d0ac8770a9f62cf49a11f2e6ecd5913a2553016
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-07-26 18:32:01 +0300

    added piexifjs to get exif metadata with more details about the image file

--- metadataExtractor.js
+++ metadataExtractor.js
@@ -3,6 +3,13 @@ const router = require("express").Router();
 
 const multer = require('multer');
 
+
+
+//For using piexifjs in photo.
+const fs = require('fs');
+const piexif = require('piexifjs');
+
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/data/uploads/')
@@ -17,9 +24,46 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 router.post('/', upload.single('uploaded_file'), async (req, res) => {
-    const metadata = await metadataExtract(req.file.path);
-    res.json(metadata);
+    const metadatasmall = await metadataExtract(req.file.path);
+    // res.json(metadata);
+
+    // Using piexifjs in addition with the metadata-extract for image files
+    if (req.file.mimetype === 'image/jpeg') {
+        const getBase64DataFromJpegFile = filename => fs.readFileSync(filename).toString('binary');
+        const getExifFromJpegFile = filename => piexif.load(getBase64DataFromJpegFile(filename));
+
+        const exifData = await getExifFromJpegFile(req.file.path);
+
+        const metadata = debugExif(exifData);
+        const finalMetadata = { ...metadatasmall, ...metadata }
+        res.json(finalMetadata);
+        return;
+    } else {
+        res.json(metadatasmall);
+        return;
+    }
 });
+
+function debugExif(exif) {
+    const finalData = {};
+    for (const ifd in exif) {
+        if (ifd == 'thumbnail') {
+            const thumbnailData = exif[ifd] === null ? "null" : exif[ifd];
+            // console.log(`- thumbnail: ${thumbnailData}`);
+            // finalData.push(`thumbnail: ${thumbnailData}`);
+            finalData.thumbnail = thumbnailData;
+        } else {
+            // console.log(`- ${ifd}`);
+            for (const tag in exif[ifd]) {
+                // console.log(`    - ${piexif.TAGS[ifd][tag]['name']}: ${exif[ifd][tag]}`);
+                // finalData.push(`${piexif.TAGS[ifd][tag]['name']}: ${exif[ifd][tag]}`);
+                finalData[`${piexif.TAGS[ifd][tag]['name']}`] = exif[ifd][tag]
+            }
+        }
+    }
+    return finalData;
+}
+
 
 
 module.exports = router;
