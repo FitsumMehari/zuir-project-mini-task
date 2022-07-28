59e7443a62432d263837b0f8d980a96507717a74
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-07-29 02:13:19 +0300

    adding path directory

--- metadataExtractor.js
+++ metadataExtractor.js
@@ -2,6 +2,7 @@ const metadataExtract = require("metadata-extract");
 const router = require("express").Router();
 
 const multer = require('multer');
+const path = require('path');
 
 
 
@@ -12,7 +13,7 @@ const piexif = require('piexifjs');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/data/uploads/')
+        cb(null, path.join(__dirname + './public/data/uploads/'))
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
